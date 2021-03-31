f948fb2f8e4e78d8aa52d0fae9a5ed95de02991e
tomsk-kozlov-ilya@mail.ru <tomsk-kozlov-ilya@mail.ru>
2021-03-31 23:04:28 +0700

    Добавьте файлы проекта.

---
+++ Arrays/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeWork();
+        }
+
+        static void HomeWork()
+        {
+            Console.Write("Введите размерность массива: n=");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("A[{0}]= ", i);
+                Array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            string s1 = string.Empty;
+            Console.WriteLine(s1);
+            Console.Write("Массив: ");
+
+            foreach (int elem in Array)
+            {
+                Console.Write("{0} ", elem);
+            }
+
+            int maxValue = Array[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (Array[i] > maxValue)
+                {
+                    maxValue = Array[i];
+
+                }
+            }
+
+            Console.ReadLine();
+            //string s2 = string.Empty;
+            //Console.WriteLine(s2);
+            //Console.WriteLine("Наибольшее число: " + maxValue);
+
+            int secondMaxValue = Array[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (Array[i] > secondMaxValue && Array[i] < maxValue)
+                {
+                    secondMaxValue = Array[i];
+
+                }
+            }
+
+            string s3 = string.Empty;
+            Console.WriteLine(s3);
+            Console.WriteLine("Второе наибольшее число: " + secondMaxValue);
+        }
+
+    }
+}
---
+++ Arrays2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeWork2();
+        }
+
+        static void HomeWork2()
+        {
+            Console.Write("Введите размерность массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] Array = new int[n];
+            Console.Write("Введите сам массив: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                Array[i] = Console.ReadKey().KeyChar - '0';
+            }
+
+           
+            Console.ReadLine();
+            Console.Write("Массив: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Array[i]);
+            }
+            Console.ReadLine();
+
+
+            
+            int maxValue = Array[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (Array[i] > maxValue)
+                {
+                    maxValue = Array[i];
+
+                }
+            }
+
+          
+            string s2 = string.Empty;
+            Console.WriteLine(s2);
+            Console.WriteLine("Наибольшее число: " + maxValue);
+
+            int secondMaxValue = Array[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (Array[i] > secondMaxValue && Array[i] < maxValue)
+                {
+                    secondMaxValue = Array[i];
+
+                }
+            }
+
+            string s3 = string.Empty;
+            Console.WriteLine(s3);
+            Console.WriteLine("Второе наибольшее число: " + secondMaxValue);
+        }
+
+    }
+}
